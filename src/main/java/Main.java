30922a41d0dec1d10f6ef13afd219afebc3efaa4
SaxsCode <maxsax.sch@gmail.com>
2025-07-13 21:02:04 +0200

    feat: add gui on work on adding number buttons

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,37 @@
+import javax.swing.*;
+import java.util.*;
+
 public class Main {
-    
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        generateGUI();
+    }
+ 
+    private static void generateGUI()
+    {
+        JFrame frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+
+        List<JButton> buttons = getButtons(frame);
+
+        for (JButton button : buttons) {
+            frame.getContentPane().add(button);
+        }
+
+        frame.setVisible(true);
+    }
+
+    private static List<JButton> getButtons(JFrame frame) {
+
+        List<JButton> buttons = new ArrayList<>();
+
+        for (int number = 0; number < 10; number++) {
+            buttons.add(new JButton(Integer.toString(number)));
+        }
+
+        return buttons;
+
     }
 
 }
